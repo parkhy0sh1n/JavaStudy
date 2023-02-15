d1847845197bed472e1863fb4045fe95bcec2a63
parkhy0sh1n <parkhy0sh1n@naver.com>
2023-02-15 17:42:08 +0900

    2023-02-15

--- Appendix03_jdbc/src/ex01_connection/MainClass.java
+++ Appendix03_jdbc/src/ex01_connection/MainClass.java
@@ -6,41 +6,55 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MainClass {
-	
+
 	public static void ex01() {
+		
 		// 클래스를 로드하는 방법(메모리에 로드한다 : 사용하겠다)
 		try {
-			Class.forName("oracle.jdbc.OracleDriver"); 	 // oracle.jdbc.OracleDriver
+			
+			Class.forName("oracle.jdbc.OracleDriver");  // oracle.jdbc.driver.OracleDriver도 가능하다.
 			System.out.println("클래스가 로드되었습니다.");
+			
 		} catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 	}
-	
+
 	public static void ex02() {
-		// oracle 접속 정보
+		
+		// Oracle 접속 정보
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "GDJ61";
 		String password = "1111";
-		// Oracle Database와 연결할 떄 사용하는 Connection 인터페이스
+		
+		// Oracle DataBase와 연결할 때 사용하는 Connection 인터페이스
 		Connection con = null;
-		// DriverManager 클래스로부터 Connection 객체를 받아 온다.
+		
+		// DriverManager 클래스로부터 Connection 객체를 받아 온다.		
 		try {
+			
 			con = DriverManager.getConnection(url, user, password);
-			System.out.println("db에 접속되었습니다.");
-		} catch(SQLException e) {
-			e.printStackTrace();              
-		}
-		// 사용이 끝 Connection 객체는 반드시 닫아야 한다.
-		try {
-			if(con != null) {
-			con.close();
-			}
+			System.out.println("DB에 접속되었습니다.");
+			
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
+		
+		// 사용이 끝난 Connection 객체는 반드시 닫아야 한다.
+		try {
+			
+			if(con != null) {
+				con.close();
+			}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public static void ex03() {
@@ -49,46 +63,65 @@ public class MainClass {
 		Connection con = null;
 		
 		try {
-			// Property 파일을 읽는 문자 입력 스트림 생성하기
+			
+			// 프로퍼티 파일을 읽는 문자 입력 스트림 생성하기
 			reader = new BufferedReader(new FileReader("db.properties"));
 			
-			// Property 파일을 읽어서 Property 객체 생성하기
-			Properties properties = new Properties;
+			// 프로퍼티 파일을 읽어서 프로퍼티 객체 생성하기
+			Properties properties = new Properties();
 			properties.load(reader);
 			
-			// Property 객체에 저장된 각 Property 읽기
+			// 프로터티 객체에 저장된 각 Property 읽기
 			String url = properties.getProperty("url");
 			String user = properties.getProperty("user");
 			String password = properties.getProperty("password");
 			
 			// DriverManager로부터 Connection 객체 얻기
-			try {
-				con = DriverManager.getConnection(properties.toString());
-			} catch (SQLException e) {
-			
-				e.printStackTrace();
-			}
+			con = DriverManager.getConnection(url, user, password);
 			System.out.println("DB에 접속되었습니다.");
 			
-			
 		} catch(IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
+			
 			try {
 				if(con != null) {
 					con.close();
 				}
-			} catch(SQLException e) {
+			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+			
 		}
 		
 	}
-
+	
+	public static Connection getConnection() {
+		
+		Connection con = null;
+		
+		try {
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+			Properties properties = new Properties();
+			properties.load(new BufferedReader(new FileReader("db.properties")));
+			
+			con = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
+			
+		} catch (Exception e) {  // ClassNotFoundException, SQLException, IOException
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
+	
 	public static void main(String[] args) {
-		ex03();
+		Connection con = getConnection();
+		System.out.println("DB에 접속되었습니다.");
 	}
 
 }
