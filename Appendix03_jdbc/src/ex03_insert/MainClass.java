b0336a5cb62daa57c67e854de37854c6848fab7f
parkhy0sh1n <parkhy0sh1n@naver.com>
2023-02-17 09:01:57 +0900

    2023-02-17

--- Appendix03_jdbc/src/ex03_insert/MainClass.java
+++ Appendix03_jdbc/src/ex03_insert/MainClass.java
@@ -38,11 +38,68 @@ public class MainClass {
 	
 	// 변수 삽입
 	public static void ex02() {
-		
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			Properties p = new Properties();
+			p.load(new BufferedReader(new FileReader("db.properties")));
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			con = DriverManager.getConnection(url, p);
+			String sql = "INSERT INTO MEMBER_TBL(MEMBER_NO, ID, NAME, ADDRESS, JOIN_DATE)";
+			sql += " VALUES(MEMBER_SEQ.NEXTVAL, ?, ?, ?, sysdate)";
+			ps = con.prepareStatement(sql);
+			String id = "master";
+			String name = "운영자";
+			String address = "경기";
+			ps.setString(1, id); 		// 1번째 물음표(?) 자리에 id를 넣으시오.
+		    ps.setString(2, name);      // 2번째 물음표(?) 자리에 name를 넣으시오.
+		    ps.setString(3, address);   // 3번째 물음표(?) 자리에 address를 넣으시오.
+		    int insResult = ps.executeUpdate();
+		    System.out.println(insResult + "개의 행이 삽입되었습니다.");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void ex03() {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			Properties p = new Properties();
+			p.load(new BufferedReader(new FileReader("db.properties")));
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			con = DriverManager.getConnection(url, p);
+			String sql = "INSERT INTO BOARD_TBL(BOARD_NO, MEMBER_NO, TITLE, CONTENT, CREATE_DATE)";
+				sql += " VALUES(BOARD_SEQ.NEXTVAL, ?, ?, ?, SYSDATE)";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, 1);
+			ps.setString(2, "김민서의 하루");
+			ps.setString(3, "너무 졸리다");
+			int insResult = ps.executeUpdate();
+			System.out.println(insResult + "개의 행이 삽입되었습니다.");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(ps != null) ps.close();
+				if(con != null) con.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
-		ex01();
+		ex03();
 		
 	}
 
